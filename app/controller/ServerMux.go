7019e4cabf5879959075c14f86e9c3b3f4bbf136
Philipp Kalytta <github@kalytta.net>
2018-06-06 16:58:35 +0200

    Added Bookmarked False
    Signed-off-by: Philipp Kalytta <github@kalytta.net>

--- app/controller/ServerMux.go
+++ app/controller/ServerMux.go
@@ -638,7 +638,7 @@ func getRentedEquip(db *sql.DB, UserID int64, bookmarked bool) *[]equipmentData 
 	var err error
 
 	if (!bookmarked) {
-		rows, err = db.Query("SELECT * FROM equipment e WHERE e.RentedbyUserID = " + strconv.FormatInt(UserID, 10) + ";")
+		rows, err = db.Query("SELECT * FROM equipment e WHERE e.RentedbyUserID = " + strconv.FormatInt(UserID, 10) + " AND e.Bookmarked = false;")
 	} else {
 		rows, err = db.Query("SELECT * FROM equipment e WHERE e.RentedbyUserID = " + strconv.FormatInt(UserID, 10) + " AND e.Bookmarked = true;")
 	}
